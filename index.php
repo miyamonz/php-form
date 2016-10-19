14695c24bbc09f6860c3dd366800889aae0db2fc
Koichi Miyamoto <m5813k@gmail.com>
2016-10-20 00:24:00 +0900

    fix index.php

--- index.php
+++ index.php
@@ -17,10 +17,10 @@ require_once(__DIR__ . "/"."util/util.php");
     <form class="h-adr" action="form.php" method="post">
       ※ は必須項目です
       <table>
-        <tr><td>名前 ※：</td><td><input type="text" name="person_name"></td></tr>
-        <tr><td>ふりがな ※：</td><td><input type="text" name="person_furigana"></td></tr>
-        <tr><td>メールアドレス ※：</td><td><input type="text" name="email1"></td></tr>
-        <tr><td>メールアドレス(確認) ※：</td><td><input type="text" name="email2"></td></tr>
+        <tr><td>名前 ※</td><td><input type="text" name="person_name"></td></tr>
+        <tr><td>ふりがな ※</td><td><input type="text" name="person_furigana"></td></tr>
+        <tr><td>メールアドレス ※</td><td><input type="text" name="email1"></td></tr>
+        <tr><td>メールアドレス(確認) ※</td><td><input type="text" name="email2"></td></tr>
         <tr><td>会社名 ※</td><td><input type="text" name="company_name"></td></tr>
         <tr><td>役職</td>
           <td>
@@ -46,32 +46,32 @@ require_once(__DIR__ . "/"."util/util.php");
         <!-- 郵便番号-->
         <input type="hidden" class="p-country-name" value="Japan">
         <tr> 
-          <td>郵便番号 ※：</td>
+          <td>郵便番号 ※</td>
           <td>
             <input class="p-postal-code" type="text" size="3" maxlength="3" name="zip1" placeholder="012"> -
             <input class="p-postal-code" type="text" size="4" maxlength="4" name="zip2" placeholder="3456">
           </td>
         </tr>
-        <tr><td>都道府県 ※：</td><td><input    class="p-region"           type="text" name="prefecture" placeholder=""></td></tr>
-        <tr><td>市区町村 ※：</td><td><input    class="p-locality"         type="text" name="city"       placeholder=""></td></tr>
-        <tr><td>住所1(番地) ※：</td><td><input class="p-street-address"   type="text" name="address1"></td></tr>
-        <tr><td>住所2(建物名)：</td><td><input class="p-extended-address" type="text" name="address2"></td></tr>
+        <tr><td>都道府県 ※</td><td><input    class="p-region"           type="text" name="prefecture" placeholder=""></td></tr>
+        <tr><td>市区町村 ※</td><td><input    class="p-locality"         type="text" name="city"       placeholder=""></td></tr>
+        <tr><td>住所1(番地) ※</td><td><input class="p-street-address"   type="text" name="address1"></td></tr>
+        <tr><td>住所2(建物名)</td><td><input class="p-extended-address" type="text" name="address2"></td></tr>
         <br>
-        <tr><td>電話番号 ※：</td><td><input type="text" name="phone_number" placeholder="012-345-6789"></td></tr>
-        <tr><td>FAX番号：</td><td><input type="text" name="fax_number" placeholder="012-345-6789"></td></tr>
+        <tr><td>電話番号 ※</td><td><input type="text" name="phone_number" placeholder="012-345-6789"></td></tr>
+        <tr><td>FAX番号</td><td><input type="text" name="fax_number" placeholder="012-345-6789"></td></tr>
       </table>
       <br>
       <table>
         <ul>
           <li> 
-          お問い合わせ項目 ※：<br>
+          お問い合わせ項目 ※<br>
           <?php for($i=0; $i<count($aboutVals); $i++){ ?>
           <input type="radio" name="about" value="<?= $aboutVals[$i]["value"]?>"><?= $aboutVals[$i]["text"]?><br>
           <?php } ?>
 
           </li>
           <li> 
-          お問い合わせ内容 ※：<br>
+          お問い合わせ内容 ※<br>
           <textarea type="text" name="naiyou" rows="7" cols="50"></textarea>
 
           </li>
